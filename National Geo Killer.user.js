a417e4b2a0ab2f2cd55d82d7de567ddd097433bc
AliceSpectre <121159767+AliceSpectre@users.noreply.github.com>
2022-12-21 17:20:17 +0800

    Add files via upload

---
+++ National Geo Killer.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         National Geographic Email Signup Wall Blocker
+// @namespace    http://tampermonkey.net/
+// @license      MIT
+// @version      0.1.5
+// @description  Active checks for the existance of the 'email wall', remove changes to page for easy viewing.
+// @author       https://github.com/AliceSpectre
+// @match        https://www.nationalgeographic.com/*
+// @match        https://*.nationalgeographic.com/*
+// @match        www.nationalgeographic.com/*
+// @match        http://www.nationalgeographic.com/*
+// @match        http://*.nationalgeographic.com/*
+
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nationalgeographic.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Bypass National Geography Email 'PAYWALL'
+    function paywall_killer() {
+        // Step 1 - Remove body attributes to allow content viewing & scrolling
+        console.log("++ Removing BODY class");
+        document.getElementsByTagName("body")[0].removeAttribute("class");
+        console.log("++ Removing BODY style");
+        document.getElementsByTagName("body")[0].removeAttribute("style");
+        // Step 2 - Remove Bullshit Box`
+        console.log("++ Removing Bullshit signup box.");
+        document.getElementById("fittPortal_0").remove()
+    }
+
+    function Val_Update() {
+        var element = document.getElementById("fittPortal_0");
+
+        //If it isn't "undefined" and it isn't "null", then it exists.
+        if(typeof(element) != 'undefined' && element != null){
+            console.log('Pay/Email Wall detected here');
+            paywall_killer()
+        } else{
+            console.log("No Pay/Email Wall detected here");
+            console.log("Rechecking in 5 seconds")
+        }
+    }
+
+    setInterval(function() {
+        console.clear()
+        Val_Update();
+    }, 1000);
+})();
